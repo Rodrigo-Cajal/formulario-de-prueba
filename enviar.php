c1e492c35a0c87699f39b5785592225f53956608
Rodrigo Cajal <rodrigoecajal@gmail.com>
2023-07-19 16:24:12 -0300

    cambios envio

--- enviar.php
+++ enviar.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nombre = $_POST['nombre'];
     $email = $_POST['mail'];
     $celular = $_POST['celular'];
     $mensaje = $_POST['mensaje'];
 
-    $destinatario = 'rodriezecajal@hotmail.com';
-    $asunto = 'Formulario de prueba';
+    $destinatario = 'loftagencia23@gmail.com';
+    $asunto = 'Consultas Agencia Loft';
 
     $contenido = "Nombre: $nombre\n";
     $contenido .= "Email: $email\n";
